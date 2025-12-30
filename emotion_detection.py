7eb32f5397a52ec4dfb200eb3009a35485f72dc8
thelandlord92 <bayo@onile.ai>
2025-12-30 09:41:21 -0500

    Emotion detection function

--- emotion_detection.py
+++ emotion_detection.py
@@ -22,4 +22,28 @@ def emotion_detector(text_to_analyze):
     # Sending a POST request to the emotion analysis API
     response = requests.post(url, json = myobj, headers=headers)
 
-    return response.text
+    # Parsing the JSON response from the API
+    response_json = json.loads(response.text)
+
+    # Get the emotion dictionary
+    emotion_dict = response_json["emotionPredictions"][0]["emotion"]
+
+    # Sort the emotions based on the scores to find the highest value
+    emotion_list = []
+    # Switch the position of the keys and values to (value, key)
+    for key, value in emotion_dict.items():
+        tup = (value, key)
+        emotion_list.append(tup)
+    sorted_emotion_list = sorted(emotion_list, reverse=True)
+
+    # Created a formatted dictionary of the emotions
+    formatted_emotion_dict = {
+        "anger": emotion_dict["anger"],
+        "disgust": emotion_dict["disgust"],
+        "fear": emotion_dict["fear"],
+        "joy": emotion_dict["joy"],
+        "sadness": emotion_dict["sadness"],
+        "dominant_emotion": sorted_emotion_list[0][1]
+    }
+
+    return formatted_emotion_dict
